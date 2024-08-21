b3665c4d1dd85445a98d63aa67cd9d736c00190c
kurniaaa01 <husnulkurnia299@gmail.com>
2024-08-21 16:37:04 +0700

    menambahkan tugas 13

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
+
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'processRegister'])->name('register.submit');
+
 Route::get('/welcome', [AuthController::class, 'welcome'])->name('welcome');
+
+route::get('/table',function(){
+    return view('table');
+});
+route::get('/data-table',function(){
+    return view('data-table');
+});
 
 
