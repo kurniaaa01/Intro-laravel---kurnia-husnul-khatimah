9f279eb4040e58185489f11ce2fd4aff187e55b8
kurniaaa01 <husnulkurnia299@gmail.com>
2024-08-23 16:32:16 +0700

    menambahkan tugas 15

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CastController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,5 +18,14 @@ route::get('/table',function(){
 route::get('/data-table',function(){
     return view('data-table');
 });
+
+Route::get('/cast', [CastController::class, 'index']);
+Route::get('/cast/create', [CastController::class, 'create']);
+Route::post('/cast', [CastController::class, 'store']);
+Route::get('/cast/{cast_id}', [CastController::class, 'show']);
+Route::get('/cast/{cast_id}/edit', [CastController::class, 'edit']);
+Route::put('/cast/{cast_id}', [CastController::class, 'update']);
+Route::delete('/cast/{cast_id}', [CastController::class, 'destroy']);
+
 
 
